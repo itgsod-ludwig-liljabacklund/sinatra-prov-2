b4bd6fd41045d00c081171ed821d67313b463ed7
itgsod-ludwig-liljabacklund <ludwig.liljabacklun@LLB-Macbook.local>
2015-03-12 10:44:07 +0100

    added book_showing code in app

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
     @movie_id = movie_id
     @showings = Showing.all(movie_id: movie_id)
     @cinemas_showing_movie = []
-    for showing in @showings
+    for showing in @showings #Hämtar alla biografer som visar filmen
       if showing.movie_id == @movie_id.to_i
         @cinemas_showing_movie << (Cinema.first(id: showing.cinema_id))
       end
@@ -32,6 +32,19 @@ class App < Sinatra::Base
     @movies_in_cinema = showings_in_cinema.movies
     p @movies_in_cinema
     slim :cinema
+  end
+
+  get '/book_showing/:id' do |showing_id|
+    @showing = Showing.get(showing_id)
+    @movie = Movie.first(id: @showing.movie_id)
+    @cinema = Cinema.first(id: @showing.cinema_id)
+    @room = Room.first(id: @showing.room_id)
+    slim :book_showing
+  end
+
+  post '/book_showing/:id' do |showing_id|
+    Booking.create(name: params['name'], showing_id: showing_id)
+    redirect '/'
   end
 
 end
