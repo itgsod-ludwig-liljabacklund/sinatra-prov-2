80cbf6be17792988fc96baaf2e82a2c583e4de25
itgsod-ludwig-liljabacklund <ludwig.liljabacklun@LLB-Macbook.local>
2015-03-12 09:48:11 +0100

    added cinema browser for movie code in app

--- app.rb
+++ app.rb
@@ -3,10 +3,20 @@ class App < Sinatra::Base
 
   get '/' do
     @movies = Movie.all
-    p @movies
     @cinemas = Cinema.all
-    p @cinemas
     slim :index
+  end
+
+  get '/movie/:id' do |movie_id|
+    @movie_id = movie_id
+    @showings = Showing.all(movie_id: movie_id)
+    @cinemas_showing_movie = []
+    for showing in @showings
+      if showing.movie_id == @movie_id.to_i
+        @cinemas_showing_movie << (Cinema.first(id: showing.cinema_id))
+      end
+    end
+    slim :movie
   end
 
 
