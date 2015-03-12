c2ca91cb477ad0d17079b005540a2f5a9699d524
itgsod-ludwig-liljabacklund <ludwig.liljabacklun@LLB-Macbook.local>
2015-03-12 09:04:14 +0100

    added movie model

---
+++ models/movie.rb
@@ -0,0 +1,9 @@
+class Movie
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :name, String, :required => true
+  property :runtime, Integer, :required => true
+
+  has n, :showing
+end
