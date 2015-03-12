59eb9f698525ab789b625cecd91a5f58242d1055
itgsod-ludwig-liljabacklund <ludwig.liljabacklun@LLB-Macbook.local>
2015-03-12 09:10:21 +0100

    changed integer to datetime in showing model

--- models/showing.rb
+++ models/showing.rb
@@ -2,7 +2,7 @@ class Showing
   include DataMapper::Resource
 
   property :id, Serial
-  property :start_time, Integer, :required => true
+  property :start_time, DateTime, :required => true
 
   belongs_to :movie
   belongs_to :cinema
