8e10fde7b66623bf4ee65caab31e5cf5af721f26
itgsod-ludwig-liljabacklund <ludwig.liljabacklun@LLB-Macbook.local>
2015-03-12 09:04:23 +0100

    added showing model

--- models/showing.rb
+++ models/showing.rb
@@ -1,2 +1,10 @@
 class Showing
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :start_time, Integer, :required => true
+
+  belongs_to :movie
+  belongs_to :cinema
+  belongs_to :room
 end
